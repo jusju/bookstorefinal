80d4cb63dd299011f158bddfa02c5a0b637b2d14
rikvart <35219277+rikvart@users.noreply.github.com>
2022-03-27 21:38:36 +0300

    Delete src directory

--- src/main/java/com/example/model/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.model;
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-	List<Book> findByTitle(String title);
-}
